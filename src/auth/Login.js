7715597524bed1bf8a63e422d6b99851e75bf55b
jida <jakazzy@gmail.com>
2019-06-09 05:35:30 +0000

    set up login branch

---
+++ src/auth/Login.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+
+class Login extends Component{
+
+  render(){
+    return(
+      <div>
+        Hello everyone
+      </div>
+    )
+  }
+
+}
+export default Login;
