434ee2cf0649a263a955df06bcecadad95e07a5a
jida <jakazzy@gmail.com>
2019-06-09 20:42:33 +0000

    remove token inorder  to logout

--- src/Logout.js
+++ src/Logout.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Logout extends Component{
-   
+  constructor(props){
+      super(props)
+      localStorage.removeItem("token");
+  } 
 render(){
     return(
         <div>
