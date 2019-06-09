353fc3c96ea862025e063135da1b01b808ef50ef
jida <jakazzy@gmail.com>
2019-06-09 19:52:49 +0000

    edit Logout file

--- src/Logout.js
+++ src/Logout.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const Logout =() =>{
+class Logout extends Component{
+   
+render(){
     return(
         <div>
             <h2>You are currently Logged out</h2>
+            <Link to="/">Login to access dashboard</Link>
         </div>
-    )
-}
+        )
+        }
+    }
 
 export default Logout
