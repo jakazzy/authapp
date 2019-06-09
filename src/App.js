605b6d61db52d4940950837d6a47b8c55bff1c7e
jida <jakazzy@gmail.com>
2019-06-09 06:09:07 +0000

    add signup form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import Login from './auth/Login'
 
 
 class App extends Component{
@@ -6,7 +7,7 @@ class App extends Component{
   render(){
     return(
       <div>
-        Hello everyone
+       <Login/>
       </div>
     )
   }
