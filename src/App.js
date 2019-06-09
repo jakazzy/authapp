4d302eb3a81813f72a11c21ec986f8f625beaed3
jida <jakazzy@gmail.com>
2019-06-09 08:53:08 +0000

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component} from 'react';
-import Login from './auth/Login'
+import Login from './auth/Login';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component{
 
   render(){
     return(
-      <div>
-       <Login/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Login/>
+        </div>
+      </BrowserRouter>
+      
     )
   }
 
