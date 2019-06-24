c6bd0e425847372cd80ceaa0f00d18e7d5bdb6e8
jida <jakazzy@gmail.com>
2019-06-24 04:31:04 +0000

    add snapshot test for home component

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import { BrowserRouter,Route, Link } from 'react-router-dom';
 import { render, cleanup, fireEvent } from '@testing-library/react'
 import 'jest-dom/extend-expect'
 import Home from './../components/Home'
+
+afterEach(cleanup);
+
+test('renders home page', () => {
+    const { asFragment} = render(
+                                    <BrowserRouter>
+                                    <Route path="/" component={ Home} />
+                                        {/* <Link to="/"><Home/></Link> */}
+                                    </BrowserRouter>);
+    expect(asFragment()).toMatchSnapshot();
+})
